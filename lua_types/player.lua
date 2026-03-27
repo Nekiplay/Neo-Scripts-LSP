c10a56d75400972fc90a23be1e3c8893c46d35cb
Roman Danilov <neci.03@mail.ru>
2026-03-27 19:44:25 +0500

    Update player.lua

--- lua_types/player.lua
+++ lua_types/player.lua
@@ -22,6 +22,11 @@ local player = {}
 --- @return boolean Return true if success
 function player.addMessage(text) end
 
+---Add local message
+--- @param yaw number
+--- @param pitch number
+function player.setRotation(yaw, pitch) end
+
 --- Send message to server
 --- @param text string -- message
 --- @return boolean Return true if success
