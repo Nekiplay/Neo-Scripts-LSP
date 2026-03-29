2e3b85aa221297de7e49ad3f3a54bc860247fa95
Roman Danilov <neci.03@mail.ru>
2026-03-29 22:04:05 +0500

    Update blockpos.lua

--- lua_types/datatypes/blockpos.lua
+++ lua_types/datatypes/blockpos.lua
@@ -4,3 +4,39 @@
 --- @field x integer
 --- @field y integer
 --- @field z integer
+local blockpos = {}
+--- @return vector3d
+function blockpos.bottomCenter() end
+
+--- @return vector3d
+function blockpos.center() end
+
+--- @return blockpos
+function blockpos.above() end
+
+--- @return blockpos
+function blockpos.below() end
+
+--- @return blockpos
+function blockpos.east() end
+
+--- @return blockpos
+function blockpos.north() end
+
+--- @return blockpos
+function blockpos.west() end
+
+--- @return blockpos
+function blockpos.south() end
+
+--- @param x integer
+--- @param y integer
+--- @param z integer
+--- @return blockpos
+function blockpos.distSqr(x, y, z) end
+
+--- @param x number
+--- @param y number
+--- @param z number
+--- @return blockpos
+function blockpos.distToCenterSqr(x, y, z) end
