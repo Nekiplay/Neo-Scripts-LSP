4fcea71863356664dbe8b2f982520c4787a24110
Roman Danilov <neci.03@mail.ru>
2026-03-31 09:37:24 +0500

    Update raycastresult.lua

--- lua_types/datatypes/raycastresult.lua
+++ lua_types/datatypes/raycastresult.lua
@@ -4,5 +4,5 @@
 --- @field type string block or entity or miss
 --- @field data entity? Entity data only for entity type
 --- @field location vector3d coordinate of the end of the ray only for block type
---- @field size direction? direction only for block type
+--- @field side direction? direction only for block type
 --- @field blockPos blockpos? block postion only for block type
